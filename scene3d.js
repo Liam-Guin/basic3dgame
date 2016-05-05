915454eb6508c803cca4fbb628b0d9f564f88010
Liam-Guin <liam.guinnane@gmail.com>
2016-05-05 20:58:21 +0100

    Update scene3d.js

--- scene3d.js
+++ scene3d.js
@@ -11,10 +11,10 @@ Scene3d.prototype.init = function() {
     this.h = canvas.height;
     this.count = 50;
     this.sz = 100;
-    this.ht = 0.15; // height of person
+    this.ht = 0.25; // height of person
     this.vis = 200; // needs to be greater than the diagonal of a 100 square
     this.scrDist = 0.3; // = depth of field
-    this.scrHalf = 0.5;
+    this.scrHalf = 0.1;
     this.initField(600, 50); // # of trees, # of gifts
     this.initImages();
 
