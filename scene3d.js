768da4966065847b3d4bf713ee639463cf68f279
Liam-Guin <liam.guinnane@gmail.com>
2016-05-05 20:44:19 +0100

    Update scene3d.js

--- scene3d.js
+++ scene3d.js
@@ -11,7 +11,7 @@ Scene3d.prototype.init = function() {
     this.h = canvas.height;
     this.count = 50;
     this.sz = 100;
-    this.ht = 5;
+    this.ht = 2; // height of person
     this.vis = 200; // needs to be greater than the diagonal of a 100 square
     this.scrDist = 0.2; // = depth of field
     this.scrHalf = 0.2;
